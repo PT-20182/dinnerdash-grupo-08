b294a115fa34258ce63c2bb58d95aded858ae558
ArturHugo <arturpereira@cjr.org.br>
2018-11-21 13:44:16 -0200

    Trying to solve login bug.

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,27 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   # Validates presence and uniqueness of full name and username
-  validates_presence_of :full_name
-  validates_uniqueness_of :full_name
-  validates_presence_of :username
-  validates_uniqueness_of :username
+  validates :full_name, presence: :true, uniqueness: { case_sensitive: false }
+  validates :username, presence: :true, uniqueness: { case_sensitive: false }
+
+  # Only allow letter, number, underscore and punctuation for the username,
+  # so it can't look like another user's email
+  validates_format_of :username, with: /^[a-zA-Z0-9_\.]*$/, :multiline => true
+
+  # Virtual attribute for login so it can log using either it's email or it's username
+  attr_writer :login
+
+  def login
+    @login || self.username || self.email
+  end
+
+  # Authentication method using the login attribute.
+  def self.find_for_database_authentication(warden_conditions)
+    conditions = warden_conditions.dup
+    if login = conditions.delete(:login)
+      where(conditions.to_h).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+    elsif conditions.has_key?(:username) || conditions.has_key?(:email)
+      where(conditions.to_h).first
+    end
+  end
 end
