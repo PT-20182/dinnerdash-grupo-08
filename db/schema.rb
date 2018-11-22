4372b17478aac2edeed7476ce47a80d1452915b6
ArturHugo <arturpereira@cjr.org.br>
2018-11-22 13:39:29 -0200

    Added admin variable to users table. Fixed the routes.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_21_151454) do
+ActiveRecord::Schema.define(version: 2018_11_22_145028) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2018_11_21_151454) do
     t.datetime "updated_at", null: false
     t.string "username"
     t.string "full_name"
+    t.boolean "admin"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["full_name"], name: "index_users_on_full_name", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
