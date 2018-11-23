f518dcbe1937dc1aa99cea3743bbb66bdac2ae50
Fernanda <fernandamacedo.etb@gmail.com>
2018-11-22 23:28:06 -0200

    Create functionality implemented

--- app/controllers/situations_controller.rb
+++ app/controllers/situations_controller.rb
@@ -8,8 +8,22 @@ class SituationsController < ApplicationController
   end
 
   def new
+    @situation = Situation.new
   end
 
+  def create
+    situation = Situation.create(situation_params)
+
+    redirect_to situations_path
+  end
+  
   def edit
   end
+
+  private
+
+  def situation_params
+    params.require(:situation).permit(:description)
+  end
+
 end
