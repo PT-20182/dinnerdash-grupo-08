6928d9c6dffc42569d4df87dd7883b9c16e1902c
Fernanda <fernandamacedo.etb@gmail.com>
2018-11-22 23:11:08 -0200

    Read functionality has implemented

--- app/controllers/situations_controller.rb
+++ app/controllers/situations_controller.rb
@@ -1,8 +1,10 @@
 class SituationsController < ApplicationController
   def index
+    @situations = Situation.all
   end
 
   def show
+    @situation = Situation.find(params[:id])
   end
 
   def new
