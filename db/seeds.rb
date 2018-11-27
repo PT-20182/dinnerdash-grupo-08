819c00383a0cbbee4a2f21719513bd2dd8e44e96
Carol Ferreira <carolineferreirap@gmail.com>
2018-11-27 10:33:29 -0200

    CRUD categoria

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Category.create(name: 'Salada')
+Category.create(name: 'Massa')
+Category.create(name: 'Acompanhamento')
+Category.create(name: 'Sobremesa')
