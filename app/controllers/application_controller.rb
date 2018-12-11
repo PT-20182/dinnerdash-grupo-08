8ba0be1716b4c340c802ff790c456ddb6116f0c0
JoaoVictorCabraldeMelo <joao.victor.cabralmelo@gmail.com>
2018-12-11 20:56:30 -0200

    Terminando o parte de pedidos

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,17 @@
 class ApplicationController < ActionController::Base
 
+    helper_method :current_order
+
+    def current_order
+        if !session[:order_id].nil?
+            Order.find[session[:order_id]]
+        else
+            Order.new
+        end
+    end
+
     def check_admin_status
-        unless user_signed_in? && current_user.admin? 
+        unless user_signed_in? && current_user.admin?
             redirect_to root_path
             flash[:warning] = "Permission denied. You have no administrator permissions!"
         end
--- app/controllers/order_meals_controller.rb
+++ app/controllers/order_meals_controller.rb
@@ -1,16 +1,23 @@
 class OrderMealsController < ApplicationController
-    def index
-    end
-  
-    def show
-    end
-  
-    def edit
-    end
-  
-    def new
-    end
-  
-    def destroy
-    end
+  def index
   end
+
+  def create
+        @order      = current_order
+        @order_meal = @order.order_meals.new
+  end
+
+
+  def show
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  private 
+
+
+end
