5e295df49311a5fb77b583a2a432d5b7fa8ce957
ArturHugo <arturpereira@cjr.org.br>
2018-12-11 20:40:15 -0200

    funcao de deletar ta funcionando, a de adicionar ainda ta adicionando repetido

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -1,31 +1,53 @@
 class CartController < ApplicationController
-
+    
     before_action :set_cart
 
     def add_item
 
-       @item = {id: params[:meal_id], qtd: params[:quantity]}  
+        @item = {id: params[:meal_id], qtd: params[:quantity]}  
 
-       set_cart.push(@item)
+        @already_in = false
 
-       redirect_to meals_path
+        set_cart.each do |included_item|
+            if @item["id"] == included_item["id"]
+                @already_in = true
+                break
+            end
+        end
+
+        if @already_in
+            
+        else
+            set_cart.push(@item)
+
+        end
+
+        redirect_to root_path
 
     end
 
     def remove_item
-       @cart.delete_at(params[item].to_i)
 
-        # if tava no cardapio
-        #     redirect_to meals_path
-        # else 
-        #     redirect_to order_meals_path
-        # end 
+        set_cart.map! do |item|
+            if item["id"] == params[:meal_id]
+                if item["qtd"].to_i - params[:quantity].to_i > 0
+                    {"id" => item["id"], "qtd" => item["qtd"].to_i - params[:quantity].to_i}
+                else
+                    set_cart.delete(item)
+                    break
+                end
+            else
+                item
+            end
+        end
+
+        redirect_to root_path
     end
 
     private
-
+  
     def set_cart
-        session[:cart] ||= [] 
+        session[:cart] ||= []
     end
 
 end
